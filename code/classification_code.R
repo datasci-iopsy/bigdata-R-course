1b8623efaaf79ba42ae5f3052ae41da9bcf11e46
dkgreen24 <14611107+dkgreen24@users.noreply.github.com>
2019-11-06 12:59:13 -0500

    added classification exercise code

--- code/classification_code.R
+++ code/classification_code.R
@@ -1,5 +1,4 @@
-#keep env clean
-rm(list = ls())
+#rm(list = ls())
 
 #load appropriate libraries
 library(tidyverse)
@@ -49,7 +48,7 @@ meta_dat = meta_raw %>%
         season = factor(str_to_title(season)), 
         temp_mean = as.double(temp_mean),
         insesh_fl = parse_number(insesh_fl) / 100 #percentage
-           ) %>%
+    ) %>%
     mutate_if(is.numeric, list(as.double)) %>% 
     drop_na()
 
@@ -57,12 +56,16 @@ meta_dat = meta_raw %>%
 mrg_dat = merge(pir_dat, meta_dat, by = 'date') %>%
     select(-c(date, datetime, waittime))
 
-#summary statistics for numerical features
-sum_stats = mrg_dat %>% 
-    select_if(is.numeric) %>% 
-    sapply(function(x) broom::tidy(summary((x)))) %>% 
-    as.data.frame()
-sum_stats
+mrg_dat %>% 
+    select_if(is.numeric) %>%
+    sapply(function(x) summary(x)) %>% 
+    data.frame()
+
+# #summary statistics for numerical features using tidy; rmarkdown has issues rendering
+# sum_stats = mrg_dat %>% 
+#     select_if(is.numeric) %>% 
+#     sapply(function(x) broom::tidy(summary((x)))) %>% 
+#     as.data.frame()
 
 #normalise the data
 normalize = function(x) {
@@ -74,75 +77,80 @@ normalize = function(x) {
 #apply function to dataset
 mrg_dat_norm = mrg_dat %>% 
     mutate_if(is.numeric, normalize)
-    
 
-#required to split data
+#At this point, there are a lot of relatively large files in our environment so 
+#it's good practice to tidy up env - R relies heavily on memory availablity. 
+rm(list = ls()[grep("meta_|pir_", ls())])
+rm(mrg_dat)
+
+#required to split data and preprocessing
 library(caTools)
+library(caret)
+library(doMC) #parallelization
+registerDoMC(cores = 4)
+
 set.seed(984) #for reproducibility 
 
 sample = sample.split(mrg_dat_norm, SplitRatio = .75)
 
 #training and testing dfs
 train = subset(mrg_dat_norm, sample == TRUE)
-test = subset(mrg_dat_norm, sample == FALSE)
-
-# #complete logistic regression analysis
-# 
-# #fit includes all variables and no interactions
-# fit = glm(fct_waittime ~ ., data = train, family = 'binomial')
-# 
-# #fit2 includes all variables and two interactions
-# fit2 = glm(fct_waittime ~ . + season*hist_precip + insesh_fl*parades, data = train, 
-#            family = 'binomial')
-# 
-# summary(fit)
-# summary(fit2)
-# 
-# prop.table(table(train$fct_waittime)) %>% 
-#     round(2)
-# 
-# exp(coef(fit2))
-
-#load aret package
-library(caret)
+test = subset(mrg_dat_norm, sample == FALSE) 
 
 #run logistic model
-log_mod = caret::train(form = fct_waittime ~ ., 
+log_mod = caret::train(fct_waittime ~. + season*hist_precip + cap_lost*total_hrs, 
                        data = train, 
                        trControl = trainControl(method = 'cv', number = 5), 
                        method = 'glm', 
                        family = 'binomial')
 
 #get raw output of predictions
-log_mod_pred = predict(log_mod, test, type = 'raw')
+log_mod_pred = predict(log_mod, newdata = test, type = 'raw')
+
+#summary table of non-significant coefs
+logit_sum = summary(log_mod)$coefficients %>% 
+    round(2) %>% 
+    as.data.frame() %>% 
+    rownames_to_column() %>% 
+    select(-4) %>% 
+    rename('Betas' = 1, 'Est' = 2, 'p_Value' = 4) %>% 
+    filter(p_Value > .05)
+logit_sum
 
 #function to calculate model accuracy
 calc_acc = function(actual, predicted) {
     mean(actual == predicted)
 }
 
-# 64.2% % accuracy
+# 64.4% % accuracy
 calc_acc(actual = test$fct_waittime, 
          predicted = predict(log_mod, newdata = test))
 
-#confusion matrix
-confusionMatrix(log_mod_pred, test$fct_waittime)
+#preprocess predictors - knn needs num values for factors
+dummy_coded = dummyVars(fct_waittime ~ ., data = mrg_dat_norm)
+
+dummies = data.frame(predict(dummy_coded, newdata = mrg_dat_norm)) %>% 
+    #the 'fct_waittime' factor is removed; dummy coding is f-1 
+    #combine criterion with new dataset
+    cbind('fct_waittime' = as.factor(mrg_dat_norm$fct_waittime))
+
+dummies_train = subset(dummies, sample == TRUE)
+dummies_test = subset(dummies, sample == FALSE)
 
 #knn model
-knn_mod = caret::train(fct_waittime ~ ., 
-                       data = train, 
+#define train opts
+ctrl = trainControl(method = 'cv', number = 5) #5-fold cross validation
+
+#run model
+knn_mod = caret::train(fct_waittime ~ .,  
+                       data = dummies_train, 
                        method = 'knn', 
-                       trControl= trainControl(method = 'cv', number = 5,), 
-                       tuneLength = 10)
+                       trControl = ctrl, 
+                       tuneLength = 7)
 knn_mod
 
+knn_mod_pred = predict(knn_mod, dummies_train, type = 'raw')
 
-
-
-
-
-
-
-
-
-
+#confusion matrices for both models
+confusionMatrix(log_mod_pred, test$fct_waittime)
+confusionMatrix(knn_mod_pred, x_test$fct_waittime)
