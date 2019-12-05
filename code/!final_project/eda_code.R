c567f398796c754d9489d7555ba63997fecfef33
dkgreen24 <14611107+dkgreen24@users.noreply.github.com>
2019-12-05 14:20:19 -0500

    finalized code

--- code/!final_project/eda_code.R
+++ code/!final_project/eda_code.R
@@ -1,27 +1,11 @@
 rm(list = ls()) #keep env clean
 
-#uncomment lines below if one prefers NOT to source scripts!
-# #load libraries
-# library(tidyverse)
-# library(data.table)
-
-# #read in dataset - feature attributes must be specified from csv files...
-# dat = read_csv("../../data/final_df.csv", 
-#                col_types = cols(
-#                    month = col_factor(), 
-#                    season = col_factor(), 
-#                    extra_morn = col_factor(), 
-#                    extra_eve = col_factor(), 
-#                    event = col_factor(), 
-#                    timeofday = col_factor())
-# )
-
-#load feature enginnering script
+#run the feature enginnering script
 source("feature-engineering_code.R")
 
 #df structure
 cat("The dataframe has", dim(dat)[1], "rows and", dim(dat)[2], "features")
-str(dat)
+glimpse(dat)
 
 #summary statistics of df
 select_if(dat, is.numeric) %>%
@@ -30,10 +14,17 @@ select_if(dat, is.numeric) %>%
 
 #### Outlier Analysis ####
 
+#create a df to handle updated values
+dat_cap = dat
+
 #outlier detection
-detect_outs <- function(dt, var) { 
+cappedOutliers <- function(dt, var, gtitle = NULL) {
+    gtitle = as.character(gtitle)
     var_name <- eval(substitute(var), eval(dt))
+    
+    tot <- sum(!is.na(var_name))
     na1 <- sum(is.na(var_name))
+    
     m1 <- mean(var_name, na.rm = T)
     
     par(mfrow = c(2, 2), oma = c(0, 0, 3, 0))
@@ -52,98 +43,101 @@ detect_outs <- function(dt, var) {
         x[x < (quants[1] - H)] <- caps[1]
         x[x > (quants[2] + H)] <- caps[2]
         
-        x
+        return(x)
     }
     
     var_name <- ifelse(var_name %in% outlier, cap_func(var_name), var_name)
     
     boxplot(var_name, main = "With Capped Outliers")
     hist(var_name, main = "With Capped Outliers", xlab = NA, ylab = NA)
-    title("Outlier Check", outer = TRUE)
+    
+    title(paste("Outlier Check: ", gtitle), outer = TRUE)
     
     na2 <- sum(is.na(var_name))
     
-    cat("Mean of the outliers:", 
-        round(mo, 2), "n")
+    #message("Outliers Identified: ", na2 - na1, " from ", tot, " Observations")
+    #message("Proportion (%) of Outliers: ", round((na2 - na1) / tot * 100), 2, "%")
+    message("Mean of the Outliers: ", round(mo, 2))
     
     m2 <- mean(var_name, na.rm = T)
     
-    cat("Mean without removing outliers:", 
-        round(m1, 2), "n")
-    cat("Mean if we remove outliers:", 
-        round(m2, 2), "n")
-
+    message("Mean without Capping Outliers: ", round(m1, 2))
+    message("Mean if Outliers are Capped: ", round(m2, 2))
+    
     dt[as.character(substitute(var))] <- invisible(var_name)
     assign(as.character(as.list(match.call())$dt), dt, envir = .GlobalEnv)
-    
+    message("Outliers successfully capped", "\n")
+        
     return(invisible(dt))
 }
 
-#cap outliers - disregarded features are commented out
-dat2 = detect_outs(dat, waittime)
-dat2 = detect_outs(dat, holiday_prox)
-#dat2 = detect_outs(dat, holiday_rnk)
-dat2 = detect_outs(dat, temp_mean)
-#dat2 = detect_outs(dat, insesh_fl)
-dat2 = detect_outs(dat, total_hrs)
-#dat2 = detect_outs(dat, hist_precip)
-dat2 = detect_outs(dat, cap_lost)
-#dat2 = detect_outs(dat, parades)
-#dat2 = detect_outs(dat, fireworks)
+# testList = list()
+# testList = lapply(dat_cap, boxplot)
 
+#cap outliers - disregarded features are commented out
+cappedOutliers(dat_cap, waittime, "Waittime")
+cappedOutliers(dat_cap, holiday_prox, "Holiday Proximity")
+cappedOutliers(dat_cap, temp_mean, "Mean Temperature")
+cappedOutliers(dat_cap,temp_max , "Max Temperature")
+cappedOutliers(dat_cap, temp_min, "Min Temperature")
+cappedOutliers(dat_cap, mk_total_hrs, "MK Total Hrs")
+cappedOutliers(dat_cap, ep_total_hrs, "EP Total Hrs")
+cappedOutliers(dat_cap, hs_total_hrs, "HS Total Hrs")
+cappedOutliers(dat_cap, ak_total_hrs, "AK Total Hrs")
+cappedOutliers(dat_cap, mk_cap_lost, "MK Capacity Lost")
+cappedOutliers(dat_cap, ep_cap_lost, "EP Capacity Lost")
+cappedOutliers(dat_cap, hs_cap_lost, "HS Capacity Lost")
+cappedOutliers(dat_cap, ak_cap_lost, "AK Capacity Lost")
 #####
 
-#rename df w/ capped features
-dat_fnl = dat2
-
-#keep env clean :)
-rm(dat2)
-
-#transforom data using norm function
-normalize = function(x) {
-    return(
-        (x - min(x)) / (max(x) - min(x))
-    )
-}
-
-#normalize all numeric features
-dat_norm = dat_fnl %>%
-    mutate_if(is.numeric, normalize)
+#summary statistics of capped df
+select_if(dat_cap, is.numeric) %>%
+    sapply(function(x) summary(x)) %>% 
+    round(2)
 
 #### Correlation Analysis ####
 
 shhh(GGally)
+library(corrplot)
 shhh(RColorBrewer)
 
+corrs <- cor(select_if(dat_cap, is.numeric), use="pairwise.complete.obs")
+
+cor_sorted <- as.matrix(sort(corrs[,'waittime'], decreasing = TRUE))
+CorHigh <- names(which(apply(cor_sorted, 1, function(x) abs(x)>0.5)))
+corrs <- corrs[CorHigh, CorHigh]
+
 #disregarded features: fireworks
-ggcorr(select_if(dat_fnl, is.numeric)[, -10], 
-       method = c("pairwise", "pearson"), 
-       label = TRUE, label_size = 5,
-       low = "#F21A00", high = "#3B9AB2", 
-       hjust = .75, 
-       size = 5, 
-       layout.exp = 1,
-       ) +
-    ggplot2::labs(
-        title = "Correlations of Selected Features", 
-        subtitle = "Disney\'s Pirates of the Carribean"
-        ) + 
-    ggplot2::theme(
-        plot.title = element_text(size = 20, face = "bold"), 
-        plot.subtitle = element_text(size = 14)
-    )
+corrplot(corrs, type = "lower", diag = FALSE, method = "circle", tl.pos = "ld", 
+         tl.srt = 45, tl.col = "black", tl.offset = .5, tl.cex = .75
+       # label = TRUE, label_size = 3,
+       # low = "#F21A00", high = "#3B9AB2",
+       # hjust = .65,
+       # size = 3,
+       # layout.exp = 2,
+       # angle = 300
+       )
+    # works with GGally only
+    # ggplot2::labs(
+    #     title = "Correlations of Selected Features", 
+    #     subtitle = "Disney\'s Pirates of the Carribean"
+    #     ) + 
+    # ggplot2::theme(
+    #     plot.title = element_text(size = 20, face = "bold"), 
+    #     plot.subtitle = element_text(size = 14)
+    # )
 
 #####
 
 #### EDA Viz ####
-library(cowplot)
-library(gridExtra)
+shhh(cowplot) #masks libridate::stamp
+shhh(gridExtra) #masks 'dplyr::combine
 
 #create dfs with categorical & continuous variables
 sep_vars = list(
-    cat_vars = dat_fnl[which(sapply(dat_fnl, is.factor))], 
-    cont_vars = dat_fnl[which(sapply(dat_fnl, is.numeric))]
-)
+    cat_vars = dat_cap[which(sapply(dat_cap, is.factor))], 
+    cont_vars = dat_cap[which(sapply(dat_cap, is.numeric))]
+    )
 
 #create a list with freq table for each feature
 freqList = lapply(sep_vars[["cat_vars"]], 
@@ -158,7 +152,7 @@ freqList = lapply(sep_vars[["cat_vars"]],
 freqList
 
 #function to automate lolipop plots
-plotLoli <- function(data_in, i, ...) {
+loliPlots <- function(data_in, i, ...) {
     args = list(...)
     
     data <- data_in[[i]]  
@@ -184,12 +178,15 @@ plotLoli <- function(data_in, i, ...) {
 
 #list of lolipop plots
 loliList = list(
-    p_month = plotLoli(data_in = freqList, i = "month"),
-    p_extmorn = plotLoli(data_in = freqList, i = "extra_morn"), 
-    p_time = plotLoli(data_in = freqList, i = "timeofday"),
-    p_season = plotLoli(data_in = freqList, i = "season"), 
-    p_exteve = plotLoli(data_in = freqList, i = "extra_eve"),
-    p_event = plotLoli(data_in = freqList, i = "event")
+    p_day = loliPlots(data_in = freqList, i = "day"),
+    p_month = loliPlots(data_in = freqList, i = "month"),
+    p_season = loliPlots(data_in = freqList, i = "season"),
+    p_mkevent = loliPlots(data_in = freqList, i = "mk_event"),
+    p_epevent = loliPlots(data_in = freqList, i = "ep_event"),
+    p_hsevent = loliPlots(data_in = freqList, i = "hs_event"),
+    p_akevent = loliPlots(data_in = freqList, i = "ak_event"),
+    p_time = loliPlots(data_in = freqList, i = "timeofday"),
+    p_event = loliPlots(data_in = freqList, i = "event")
     )
 
 # title of graphs
@@ -204,8 +201,12 @@ plot_grid(title, plotlist = loliList[c(1, 4, 3)], ncol = 2,
           rel_heights = c(.1, .1))
 plot_grid(title, plotlist = loliList[c(2, 5, 6)], ncol = 2, 
           rel_heights = c(.1, .1))
+plot_grid(title, plotlist = loliList[c(7, 9, 8)], ncol = 2, 
+          rel_heights = c(.1, .1))
 
 #density plots for continuous features
+library(e1071)
+
 plotDen <- function(data_in, i) {
     data <- data.frame(x = data_in[[i]], waittime = data_in$waittime)
     
@@ -233,12 +234,45 @@ doPlots <- function(data_in, fun, ii, ncol = 3) {
 }
 
 #density plots!
-doPlots(sep_vars[["cont_vars"]], fun = plotDen, ii = 2:10, ncol = 3)
+doPlots(sep_vars[["cont_vars"]], fun = plotDen, ii = 2:8, ncol = 3)
+doPlots(sep_vars[["cont_vars"]], fun = plotDen, ii = 9:15, ncol = 3)
+doPlots(sep_vars[["cont_vars"]], fun = plotDen, ii = 16:22, ncol = 3)
+doPlots(sep_vars[["cont_vars"]], fun = plotDen, ii = 23:28, ncol = 3)
 
 ##plot the waittimes for each time of day by c(month, season) possible facets?
+ggplot(dat_cap, aes(x = month, y = waittime, fill = month)) + 
+    geom_bar(position = "stack", stat = "identity") +
+    facet_wrap(~timeofday) + 
+    labs(
+        title = "Monthly Count by Time of Day", 
+        caption = "Good to be a night owl!"
+    )
 
 #histogram of response variable!
-ggplot(dat_fnl, aes(x = waittime)) + 
-    geom_histogram(col = 'black', fill = "darkgray", bins = 40, binwidth = 5) + 
+ggplot(dat_cap, aes(x = waittime)) + 
+    geom_histogram(col = 'black', fill = "darkgray", binwidth = 4.5) + 
     theme_minimal() + 
-    scale_x_continuous()
+    scale_x_continuous() + 
+    labs(
+        title = "Histogram of Response Variable", 
+        subtitle = "Wait Times", 
+        caption = "Outliers are capped!"
+    )
+
+#transforom data using norm function
+normalize = function(x) {
+    return(
+        (x - min(x)) / (max(x) - min(x))
+    )
+}
+
+#normalize all numeric features
+dat_norm = dat_cap %>%
+    mutate_if(is.numeric, normalize)
+    # add_column("date" = dat[, 1], .before = 1)
+
+#write out the final normalized df
+write_csv(dat_norm, path = "../../data/finalnorm_df.csv")
+
+rm(list=setdiff(ls(), "dat_norm"))
+
