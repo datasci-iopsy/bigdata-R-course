455197c8b63dcfab7009b1235b44d6e48d2b1faf
dkgreen24 <14611107+dkgreen24@users.noreply.github.com>
2019-12-16 12:56:48 -0500

    updated R code before Rmd file

--- code/!final_project/proj-analysis_code.R
+++ code/!final_project/proj-analysis_code.R
@@ -10,9 +10,9 @@ library(caret)
 dat = read_csv("../../data/finalnorm_df.csv", col_names = T) %>% 
     mutate_if(is.character, as.factor)
 
-#Modeling
+# Modeling
 
-#preprocess predictors - dummy code variables
+#preprocess predictors - dummy code categorical features
 dummy_coded = dummyVars(~ ., data = dat[, -1])
 dummies = data.frame(predict(dummy_coded, newdata = dat[, -1])) %>% 
     cbind('date' = dat$date)
@@ -21,24 +21,30 @@ dummies = data.frame(predict(dummy_coded, newdata = dat[, -1])) %>%
 glimpse(dummies) #all num features
 
 #using h2o package
+#install.packages("h2o")
 library(h2o)
-h2o.init(nthreads = -1, max_mem_size = "8G")
+h2o.init(nthreads = -1, max_mem_size = "8G") #use all cores and all (my) memory
 
+#create an h2o obj - df but in an env
 h2o_df = as.h2o(dummies[, -151])
 
-h2oTrain = h2o_df[1:217267, ]
-h2oTest = h2o_df[217268:236918, ]
+#find a better way to subset date by year with h2o objc
+#(< 2018 = training; >= 2018 = testing)
+h2oTrain = h2o_df[1:217267, ] #< 2018 = training
+h2oTest = h2o_df[217268:236918, ] #>= 2018 = testing
 
 response = "waittime"
 features = setdiff(colnames(h2oTrain), response)
 
+#run the null model - no parameterizing 
 gbm = h2o.gbm(x = features, y = response, training_frame = h2oTrain)
 summary(gbm) #RMSE: .15; r^2: .58
 
-h2o.rmse(h2o.performance(gbm, newdata = h2oTest)) #.22 
-h2o.r2(h2o.performance(gbm, newdata = h2oTest)) #.24
+#how well the null model works with testing data!
+h2o.rmse(h2o.performance(gbm, newdata = h2oTest)) # RMSE = .22 
+h2o.r2(h2o.performance(gbm, newdata = h2oTest)) #r2 = .24
 
-#a few parameters
+#a few parameters to augment
 gbm2 = h2o.gbm(x = features, 
                y = response, 
                training_frame = h2oTrain, 
@@ -53,12 +59,17 @@ gbm2 = h2o.gbm(x = features,
                score_tree_interval = 5 
                )
 summary(gbm2) #RMSE: .15; r^2: .60
+h2o.varimp_plot(gbm2, num_of_features = 20) #top 20 features of gbm2
 
-h2o.rmse(h2o.performance(gbm2, newdata = h2oTest)) #RMSE up to .22
-h2o.r2(h2o.performance(gbm2, newdata = h2oTest)) #r^2 down to .25 r^2
+#how well the second model predicts on testing data
+h2o.rmse(h2o.performance(gbm2, newdata = h2oTest)) #RMSE same at .22
+h2o.r2(h2o.performance(gbm2, newdata = h2oTest)) #r^2 up to .25 
 
-#fit models using "lucky" parameters
+#fit models using "lucky" parameters 
 hyper_params = list(max_depth = c(4, 6, 8, 12, 16, 20))
+
+#grid search of models specifying the sampling rates, stopping rounds, 
+# RMSE cut-off scoring, etc.
 grid = h2o.grid(
     hyper_params = hyper_params, 
     search_criteria = list(strategy = "Cartesian"), 
@@ -82,7 +93,7 @@ grid = h2o.grid(
     )
 grid
 
-#sort grid model RMSEs in ascending order
+#sort grid model RMSEs in ascending order - we want the lowest!
 sortedGrid = h2o.getGrid("depth_grid", sort_by = "rmse", decreasing = FALSE)
 sortedGrid
 
@@ -95,7 +106,7 @@ minDepth
 maxDepth = max(as.numeric(topDepths))
 maxDepth
 
-#final run with optimized hyperparameters
+#final run with optimized hyperparameters found in grid search - TRUE ML
 hyper_params_opt = list(
     max_depth = seq(4, 16, 1), 
     sample_rate = seq(.2, 1, .01), 
@@ -108,6 +119,7 @@ hyper_params_opt = list(
     histogram_type = c("UniformAdaptive","QuantilesGlobal","RoundRobin")
     )
 
+#randomly search the best strategy for the program to use to score models
 search_criterira = list(
     strategy = "RandomDiscrete", 
     # max_runtime_secs = 3600, 
@@ -118,6 +130,7 @@ search_criterira = list(
     stopping_tolerance = .001
     )
 
+#GBM using optimized hyperparameters and random search criteria scoring of trees
 grid_opt = h2o.grid(
     hyper_params = hyper_params_opt, 
     search_criteria = search_criterira, 
@@ -140,23 +153,27 @@ grid_opt = h2o.grid(
 
 ## Sort the grid models by RMSE
 sortedGrid_opt <- h2o.getGrid("final_grid", sort_by = "rmse", decreasing = FALSE)
-sortedGrid_opt
+sortedGrid_opt %>% summary()
     
-for (i in 1:5) {
-    gbm_loop <- h2o.getModel(sortedGrid@model_ids[[i]])
-    print(h2o.rmse(h2o.performance(gbm_loop, valid = TRUE)))
-}
+# for (i in 1:5) {
+#     gbm_loop <- h2o.getModel(sortedGrid_opt@model_ids[[i]])
+#     print(h2o.rmse(h2o.performance(gbm_loop, valid = TRUE)))
+# }
 
 #model inspection and test set scoring
-gbmBest <- h2o.getModel(sortedGrid@model_ids[[1]])
+gbmBest <- h2o.getModel(sortedGrid@model_ids[[1]]) #top model with lowest RMSE
+
+#performance of the model predicting training data
 print(h2o.rmse(h2o.performance(gbmBest, newdata = h2oTest))) #RMSE: .21
 
+#parameters used in the top model
 gbmBest@parameters
 
+#5-folk CV using the parameters from our top model on the ENTIRE dataset
 mod_cv <- do.call(h2o.gbm,
                  ## update parameters in place
                  {
-                     p <- gbm@parameters
+                     p <- gbmBest@parameters
                      p$model_id = NULL          ## do not overwrite the original grid model
                      p$training_frame = h2o_df  ## use the full dataset
                      p$validation_frame = NULL  ## no validation frame
@@ -164,10 +181,15 @@ mod_cv <- do.call(h2o.gbm,
                      p
                  }
 )
+#validation results using 5-fold CV
 mod_cv@model$cross_validation_metrics_summary
 
+#how does our best model predict now..
 pred_gbm = h2o.predict(gbmBest, newdata = h2oTest)
-pred_gbm #.38 prediction rate!
+
+#r^2
+pred_gbm #.38 prediction rate after all that tuning!
+
 
 
 
